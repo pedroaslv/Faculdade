586d0a33e4408076641c88611df860dc26a7e4c9
pedroaslv <pedro.eng189@gmail.com>
2024-09-23 19:31:29 -0300

    update

--- Algoritmos/Exercícios/Aula07/ex12.py
+++ Algoritmos/Exercícios/Aula07/ex12.py
@@ -3,18 +3,18 @@ seus preços como valores. A função deve retornar o nome do produto mais caro.
 
 dicio = {
     'Camisa': 100,
-    'Bola': 55,
+    'Bola': 505,
     'Boneco': 85,
     'Casaco': 325 
 }
 
 def produto_caro (dicionario):
-    mais_caro = 0
-    for produto, valor in dicionario.items():
-        if valor >= mais_caro:
-            mais_caro = valor
-        
-    return produto
+    mais_caro = max(dicio.values())
+    
+    for chave, valor in dicio.items():
+        if valor == mais_caro:
+            return f'{chave}: {valor}'
+
 
 res = produto_caro(dicio)
 
